95b4ec1b7ec4035274d11e58275762a054b7fe9c
hyggge <1695386157@qq.com>
2022-10-01 12:58:06 +0800

    Lab3:modify Printer

--- src/Compiler.java
+++ src/Compiler.java
@@ -2,18 +2,24 @@ import front_end.AST.Node;
 import front_end.lexer.Lexer;
 import front_end.lexer.TokenStream;
 import front_end.parser.Parser;
+import utils.Printer;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.PushbackInputStream;
 
 public class Compiler {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         PushbackInputStream input = new PushbackInputStream(new FileInputStream("testfile.txt"), 16);
         FileOutputStream output = new FileOutputStream("output.txt");
+        // set Printer
+        Printer.setFileOutputStream(output);
+        Printer.fileOutMode();
+        Printer.open();
+        // token analyse
         Lexer lexer = new Lexer(input);
         TokenStream tokenStream = lexer.getTokenStream();
+        // syntax analyse
         Parser parser = new Parser(tokenStream);
         Node AST = parser.parseCompUnit();
 
--- src/utils/Printer.java
+++ src/utils/Printer.java
@@ -3,12 +3,61 @@ package utils;
 import front_end.lexer.Token;
 import front_end.parser.SyntaxVarType;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Printer {
-    public static void printToken(Token token) {
-        System.out.println(token.getType() + " " + token.getName());
+    public static boolean onOff = false;
+    public static int mode = 0;
+    public static FileOutputStream fileOutputStream = null;
+
+    public static void setFileOutputStream(FileOutputStream fileOutputStream) {
+        Printer.fileOutputStream = fileOutputStream;
+    }
+
+    public static void open() {
+        onOff = true;
+    }
+
+    public static void close() {
+        onOff = false;
+    }
+
+    public static void stdOutMode() {
+        mode = 0;
+    }
+
+    public static void fileOutMode() throws Exception {
+        if (fileOutputStream == null) {
+            throw new Exception("No FileOutputStream");
+        }
+        mode = 1;
+    }
+
+    public static void printToken(Token token){
+        if (onOff) {
+            if (mode == 0) System.out.println(token.getType() + " " + token.getName());
+            else {
+                try {
+                    fileOutputStream.write((token.getType() + " " + token.getName() + "\n").getBytes());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static void printSyntaxVarType(SyntaxVarType type) {
-        System.out.println("<" + type.toString() + ">");
+        if (onOff) {
+            if (mode == 0) System.out.println("<" + type.toString() + ">");
+            else {
+                try {
+                    fileOutputStream.write(("<" + type.toString() + ">\n").getBytes());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
     }
 }
