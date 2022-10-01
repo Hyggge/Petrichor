220b6e6a5fffd9d7f1436c95fda255ffaeafad25
hyggge <1695386157@qq.com>
2022-10-01 15:41:06 +0800

    modify .gitignore

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,5 +1,6 @@
 import front_end.AST.Node;
 import front_end.lexer.Lexer;
+import front_end.lexer.Token;
 import front_end.lexer.TokenStream;
 import front_end.parser.Parser;
 import utils.Printer;
@@ -10,18 +11,35 @@ import java.io.PushbackInputStream;
 
 public class Compiler {
     public static void main(String[] args) throws Exception {
+        String arg = args[0];
+
         PushbackInputStream input = new PushbackInputStream(new FileInputStream("testfile.txt"), 16);
         FileOutputStream output = new FileOutputStream("output.txt");
         // set Printer
         Printer.setFileOutputStream(output);
         Printer.fileOutMode();
         Printer.open();
-        // token analyse
-        Lexer lexer = new Lexer(input);
-        TokenStream tokenStream = lexer.getTokenStream();
-        // syntax analyse
-        Parser parser = new Parser(tokenStream);
-        Node AST = parser.parseCompUnit();
+
+        if (arg.equals("-la")) {
+            // token analyse
+            Lexer lexer = new Lexer(input);
+            TokenStream tokenStream = lexer.getTokenStream();
+            Token token = tokenStream.read();
+            while (token != null) {
+                Printer.printToken(token);
+                token = tokenStream.read();
+            }
+
+        }
+        else if (arg.equals("-sa")) {
+            // token analyse
+            Lexer lexer = new Lexer(input);
+            TokenStream tokenStream = lexer.getTokenStream();
+            // syntax analyse
+            Parser parser = new Parser(tokenStream);
+            Node AST = parser.parseCompUnit();
+
+        }
 
         // close all streams
         input.close();
