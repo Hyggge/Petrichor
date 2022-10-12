3b71b3d632120386b728c65fe2e7f90d4bcddded
hyggge <1695386157@qq.com>
2022-10-12 18:03:08 +0800

    为MainFUncDef创建Symbol成员变量

--- src/front_end/AST/Func/MainFuncDef.java
+++ src/front_end/AST/Func/MainFuncDef.java
@@ -1,12 +1,38 @@
 package front_end.AST.Func;
 
 import front_end.AST.Node;
+import front_end.AST.TokenNode;
+import front_end.symbol.FuncSymbol;
+import utils.SymbolType;
 import utils.SyntaxVarType;
+import utils.ValueType;
 
 import java.util.ArrayList;
 
 public class MainFuncDef extends Node {
+    private FuncSymbol symbol;
+
     public MainFuncDef(int startLine, int endLine, SyntaxVarType type, ArrayList<Node> children) {
         super(startLine, endLine, type, children);
+        this.symbol = createSymbol();
+        System.out.println(this.symbol);
+    }
+
+    private FuncSymbol createSymbol() {
+        // FuncType Ident '(' [FuncFormalParams] ')' Block
+        String symbolName = ((TokenNode)children.get(1)).getToken().getValue();
+        SymbolType symbolType = SymbolType.SYMBOL_FUNC;
+        ValueType returnType = ValueType.INT;
+        ArrayList<ValueType> FParamTypes = new ArrayList<>();
+        ArrayList<Integer> FParamDims = new ArrayList<>();
+
+        // get FParamTypes and FParamDims
+        if (children.get(3) instanceof FuncFormalParams) {
+            FuncFormalParams funcFormalParams = (FuncFormalParams)children.get(3);
+            FParamTypes = funcFormalParams.getFParamTypes();
+            FParamDims = funcFormalParams.getFParamDims();
+        }
+
+        return new FuncSymbol(symbolName, symbolType, returnType, FParamTypes, FParamDims);
     }
 }
