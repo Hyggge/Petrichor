52c9762b4cff42a9e23e2b1df652668bf3b9c647
hyggge <1695386157@qq.com>
2022-10-12 17:26:35 +0800

    实现形参节点中创建符号

--- src/front_end/AST/Func/FuncFormalParam.java
+++ src/front_end/AST/Func/FuncFormalParam.java
@@ -2,6 +2,8 @@ package front_end.AST.Func;
 
 import front_end.AST.Node;
 import front_end.AST.TokenNode;
+import front_end.symbol.VarSymbol;
+import utils.SymbolType;
 import utils.SyntaxVarType;
 import utils.TokenType;
 import utils.ValueType;
@@ -9,11 +11,22 @@ import utils.ValueType;
 import java.util.ArrayList;
 
 public class FuncFormalParam extends Node {
+    private VarSymbol symbol;
+
     public FuncFormalParam(int startLine, int endLine, SyntaxVarType type, ArrayList<Node> children) {
         super(startLine, endLine, type, children);
+        this.symbol = null;
+        this.symbol = createSymbol();
+    }
+
+    public VarSymbol createSymbol() {
+        String symbolName = ((TokenNode)children.get(0)).getToken().getValue();
+        SymbolType symbolType = SymbolType.SYMBOL_VAR;
+        return new VarSymbol(symbolName, symbolType, getFParamType(), getFParamDim());
     }
 
     public ValueType getFParamType() {
+        if (symbol != null) return symbol.get
         TokenType tokenType = ((TokenNode)children.get(0)).getToken().getType();
         if (tokenType == TokenType.INTTK) return ValueType.INT;
         return null;
--- src/front_end/symbol/VarSymbol.java
+++ src/front_end/symbol/VarSymbol.java
@@ -3,13 +3,17 @@ package front_end.symbol;
 import utils.SymbolType;
 import utils.ValueType;
 
+import java.util.ArrayList;
+
 public class VarSymbol extends Symbol {
     private ValueType valueType;
     private int dim;
+    private ArrayList<Integer> lenList;
 
-    public VarSymbol(String symbolName, SymbolType symbolType, ValueType valueType, int dim) {
+    public VarSymbol(String symbolName, SymbolType symbolType, ValueType valueType, int dim, ArrayList<Integer> lenList) {
         super(symbolName, symbolType);
         this.valueType = valueType;
         this.dim = dim;
+        this.lenList = lenList;
     }
 }
