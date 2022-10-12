beb507d30fb37a58ecfad28e4264c12de77af75f
hyggge <1695386157@qq.com>
2022-10-12 20:11:19 +0800

    完成对错误c的检查

--- src/front_end/AST/Exp/LValExp.java
+++ src/front_end/AST/Exp/LValExp.java
@@ -1,12 +1,27 @@
 package front_end.AST.Exp;
 
 import front_end.AST.Node;
+import front_end.AST.TokenNode;
+import front_end.lexer.Token;
+import front_end.symbol.SymbolManager;
+import utils.ErrorType;
+import utils.Printer;
 import utils.SyntaxVarType;
 
 import java.util.ArrayList;
 
+// LValExp ==> Ident {'[' Exp ']'}
 public class LValExp extends Node {
     public LValExp(int startLine, int endLine, SyntaxVarType type, ArrayList<Node> children) {
         super(startLine, endLine, type, children);
+    }
+
+    @Override
+    public void checkError() {
+        Token ident = ((TokenNode)children.get(0)).getToken();
+        if (SymbolManager.getInstance().getSymbolByName(ident.getValue()) == null) {
+            Printer.printErrorMsg(ident.getLineNumber(), ErrorType.c);
+        }
+        super.checkError();
     }
 }
--- src/front_end/AST/Exp/UnaryExp.java
+++ src/front_end/AST/Exp/UnaryExp.java
@@ -1,12 +1,29 @@
 package front_end.AST.Exp;
 
 import front_end.AST.Node;
+import front_end.AST.TokenNode;
+import front_end.lexer.Token;
+import front_end.symbol.SymbolManager;
+import utils.ErrorType;
+import utils.Printer;
 import utils.SyntaxVarType;
 
 import java.util.ArrayList;
 
+// UnaryExp ==> PrimaryExp | Ident '(' [FuncRealParams] ')' | UnaryOp UnaryExp
 public class UnaryExp extends Node {
     public UnaryExp(int startLine, int endLine, SyntaxVarType type, ArrayList<Node> children) {
         super(startLine, endLine, type, children);
+    }
+
+    @Override
+    public void checkError() {
+        if (children.get(0) instanceof TokenNode) {
+            Token ident = ((TokenNode)children.get(0)).getToken();
+            if (SymbolManager.getInstance().getSymbolByName(ident.getValue()) == null) {
+                Printer.printErrorMsg(ident.getLineNumber(), ErrorType.c);
+            }
+        }
+        super.checkError();
     }
 }
