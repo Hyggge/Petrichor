6f17ee44db867d6476a07ad8e24bf23191d3fe82
hyggge <1695386157@qq.com>
2022-10-13 00:05:22 +0800

    完成对错误m的检查

--- src/front_end/AST/Stmt/ContinueStmt.java
+++ src/front_end/AST/Stmt/ContinueStmt.java
@@ -1,6 +1,9 @@
 package front_end.AST.Stmt;
 
 import front_end.AST.Node;
+import front_end.symbol.SymbolManager;
+import utils.ErrorType;
+import utils.Printer;
 import utils.SyntaxVarType;
 
 import java.util.ArrayList;
@@ -8,5 +11,13 @@ import java.util.ArrayList;
 public class ContinueStmt extends Stmt {
     public ContinueStmt(int startLine, int endLine, SyntaxVarType type, ArrayList<Node> children) {
         super(startLine, endLine, type, children);
+    }
+
+    @Override
+    public void checkError() {
+        if (SymbolManager.getInstance().getLoopDepth() <= 0) {
+            Printer.printErrorMsg(startLine, ErrorType.m);
+        }
+        super.checkError();
     }
 }
